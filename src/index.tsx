ff0b38a1654b4f603e49bedc6ce435bbdf7dcfd1
matt zhou <matt@cruelyouth.com>
2022-04-21 20:17:36 +0800

    up @types/react & @types@react-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import { store } from './app/store';
-import { Provider } from 'react-redux';
+import React from 'react'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
+import { createRoot } from 'react-dom/client'
 import 'rsuite/dist/rsuite.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root') as Element)
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+  </React.StrictMode>
+)
