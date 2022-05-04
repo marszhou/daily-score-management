5e8a78e6082aea4b3a5cd90e055539bf69597fbd
matt zhou <matt@cruelyouth.com>
2022-05-04 19:32:01 +0800

    list table

--- src/components/StudentList/ListTable.tsx
+++ src/components/StudentList/ListTable.tsx
@@ -3,100 +3,65 @@ import { toSvg } from 'jdenticon'
 import { ArrowReturnLeft, PencilFill } from 'react-bootstrap-icons'
 import style from './ListTable.module.scss'
 import { Student } from '../../types/student'
+import StudentRow from './StudentRow'
 
 interface ListTableProps {
   students: Array<Student>
 }
 
-const ListTable: FunctionComponent<ListTableProps> = ({students}) => {
+const ListTable: FunctionComponent<ListTableProps> = ({ students }) => {
   const svgString = toSvg('hahah', 32)
   const [selected, setSelected] = useState<Array<string>>([])
+  const [checkAll, setCheckAll] = useState(true)
   useEffect(() => {
-    setSelected(students.map(s => s.id))
-  },[])
-  console.log(selected)
+    setSelected(students.map((s) => s.id))
+  }, [])
 
   return (
     <table className={`table table-hover caption-top ${style.ListTable}`}>
       <caption>
-        已有 <input type="checkbox" name="all" id="" /> 全选
+        已有{' '}
+        <label>
+          <input
+            type="checkbox"
+            name="all"
+            id=""
+            onChange={(e) => {
+              if ((e.target as HTMLInputElement).checked) {
+                setSelected(students.map((s) => s.id))
+                setCheckAll(true)
+              } else {
+                setSelected([])
+                setCheckAll(false)
+              }
+            }}
+            checked={checkAll}
+          />{' '}
+          全选
+        </label>
       </caption>
 
       <tbody>
-        <tr>
-          <th scope="row" className={style.check}>
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="flexCheckDefault"
-              defaultChecked
-            />
-          </th>
-          <td className={style.avatar}>
-            <img
-              src={`data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`}
-              alt=''
-            />
-          </td>
-          <td style={{ verticalAlign: 'middle' }}>
-            <label className="form-check-label" htmlFor="flexCheckDefault">
-              Default checkbox
-            </label>
-          </td>
-          <td className={style.action}>
-            <button className="btn btn-success btn-sm" type="button">
-              <PencilFill />
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="flexCheckDefault"
-              defaultChecked
-            />
-          </th>
-          <td>
-            <img
-              src={`data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`}
-              alt=''
-            />
-          </td>
-          <td>
-            <input type="text" className='form-control form-control-sm'/>
-          </td>
-          <td style={{ width: 80, textAlign: 'right' }}>
-            <button className="btn btn-primary btn-sm" type="button">
-              <ArrowReturnLeft />
-            </button>
-          </td>
-        </tr>
-        <tr>
-          <th scope="row">
-            <input
-              className="form-check-input"
-              type="checkbox"
-              id="flexCheckDefault"
-              defaultChecked
-            />
-          </th>
-          <td>
-            <img
-              src={`data:image/svg+xml;utf8,${encodeURIComponent(svgString)}`}
-              alt=''
-            />
-          </td>
-          <td>
-            <input type="text" className='is-invalid form-control form-control-sm'/>
-          </td>
-          <td style={{ width: 80, textAlign: 'right' }}>
-            <button className="btn btn-primary btn-sm" type="button">
-              <ArrowReturnLeft />
-            </button>
-          </td>
-        </tr>
+        {students.map((student) => (
+          <StudentRow
+            key={student.id}
+            checked={selected.includes(student.id)}
+            student={student}
+            onChange={() => {
+              return false
+            }}
+            onSelect={(id) => {
+              if (selected.includes(id)) {
+                setSelected(selected.filter((s) => s !== id))
+                setCheckAll(false)
+              } else {
+                const nextSelected = [...selected, id]
+                setSelected(nextSelected)
+                setCheckAll(nextSelected.length === students.length)
+              }
+            }}
+          />
+        ))}
       </tbody>
     </table>
   )
